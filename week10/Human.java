0786a34edd3ce697a5367566e33beddd7adda8d1
Pon-cmu <77370431+Pon-cmu@users.noreply.github.com>
2021-02-12 20:04:57 +0700

    week10

---
+++ week10/Human.java
@@ -0,0 +1,23 @@
+// Phonkrit Chinatiworn 632115031
+package week10;
+
+public class Human {
+    public void hit(Animal animal){
+        System.out.println(animal.roar());
+    }
+
+    public static void main(String[] args) {
+        Animal dog = new Dog(); //Question 1
+        Human human = new Human();
+        System.out.println("The first calling hit(Animal)");
+        human.hit(dog); //Question 2
+
+        dog = new Cat();
+        System.out.println("The second calling hit(Animal)");
+        human.hit(dog); //Question 3
+
+        dog = new Fish();
+        System.out.println("The third calling hit(Animal)");
+        human.hit(dog); //Question 3
+    }
+}
